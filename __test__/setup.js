e79baa0cd7898028e204bb120d8e8e5e3b91f068
Sandor_Orosz <sandor_orosz@epam.com>
2022-08-09 14:43:11 +0200

    add env variables

--- __test__/setup.js
+++ __test__/setup.js
@@ -1,14 +1,18 @@
 const { setup: setupDevServer } = require('jest-dev-server');
 
+// Apply env variables.
+require('dotenv/config');
+const PORT = process.env.SLS_PORT;
+
 module.exports = async function globalSetup() {
   await setupDevServer({
     command: `npm start`,
     launchTimeout: 50000,
-    port: 3000,
+    port: PORT,
     usedPortAction: 'kill',
     waitOnScheme: {
       interval: '500',
-      resources: ['http://localhost:3000/test'],
+      resources: [`http://localhost:${PORT}/test`],
     },
   });
 };
