e8da0a1190bd8a003326076ecef3df5c8dc97136
Sandor_Orosz <sandor_orosz@epam.com>
2022-08-09 14:06:50 +0200

    add jest-html-reporters

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -2,16 +2,19 @@ import type { Config } from 'jest';
 import { baseConfig } from './jest.config';
 
 const config: Config = {
-    ...baseConfig,
-    testMatch: ['**/__test__/e2e/**/*.[jt]s?(x)'],
-    "reporters": [
-        "default",
-        ["jest-html-reporters", {
-            "publicPath": "./test-report",
-            "filename": "e2e-test-report.html",
-            "openReport": false
-        }]
-    ]
+  ...baseConfig,
+  testMatch: ['**/__test__/e2e/**/*.[jt]s?(x)'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: './test-report',
+        filename: 'e2e-test-report.html',
+        openReport: false,
+      },
+    ],
+  ],
 };
 
 export default config;
--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -2,17 +2,20 @@ import type { Config } from 'jest';
 import { baseConfig } from './jest.config';
 
 const config: Config = {
-    ...baseConfig,
-    testMatch: ['**/__test__/integration/**/*.[jt]s?(x)'],
-    globalSetup: './__test__/setup.js',
-    "reporters": [
-        "default",
-        ["jest-html-reporters", {
-            "publicPath": "./test-report",
-            "filename": "integration-test-report.html",
-            "openReport": false
-        }]
-    ]
+  ...baseConfig,
+  testMatch: ['**/__test__/integration/**/*.[jt]s?(x)'],
+  globalSetup: './__test__/setup.js',
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: './test-report',
+        filename: 'integration-test-report.html',
+        openReport: false,
+      },
+    ],
+  ],
 };
 
 export default config;
